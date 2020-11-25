eef99bcb7b62baa0bb424f18e5cd62833d229bed
manucho <manuel.blanco77@gmail.com>
2020-11-25 09:25:00 -0300

    Creando clases para el manejo de los tickets : ticket.js y ticket-list.js

---
+++ models/ticket.js
@@ -0,0 +1,15 @@
+//uuid para poder generar identificadores unicos
+const { v4: uuidv4 } = require('uuid');
+
+class Ticket {
+
+    constructor( number ) {
+        this.id         = uuidv4();
+        this.number     = number;
+        this.escritorio = null;
+        this.agente     = null;
+
+    }
+}
+
+module.exports = Ticket;
